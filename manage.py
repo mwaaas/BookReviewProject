329d08d3ad556c616e458e56b5dcdc84a5e74085
Francis Mwangi <francismwangi152@gmail.com>
2022-05-12 04:50:57 +0000

    adding server implementation

--- manage.py
+++ manage.py
@@ -1,24 +1,35 @@
 import argparse
 import sys
 from users import login, register
+from server import server
 
 if __name__ == '__main__':
     """ You can put your testing logic here """
-    my_parser = argparse.ArgumentParser()
-    my_parser.add_argument('action',
-                       metavar='action',
-                       type=str,
-                       help='the path to list')
-    my_parser.add_argument('--username', action='store', type=str, required=True)
-    my_parser.add_argument('--password', action='store', type=str, required=True)
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(help="Sub commands", dest="action")
 
-    if len(sys.argv) >= 2 and sys.argv[1] == "user_register":
-        my_parser.add_argument('--password2', action='store', type=str, required=True)
-        my_parser.add_argument('--dateOfBirth', action='store', type=str, required=True)
+    # registration command configuration
+    user_register_parser = subparser.add_parser("user_register", help="Command for registering user")
+    user_register_parser.add_argument('--username', action='store', type=str, required=True, help="username to use for registration")
+    user_register_parser.add_argument('--password', action='store', type=str, required=True, help="password to use for registraion")
+    user_register_parser.add_argument('--password2', action='store', type=str, required=True, help="password matching password one")
+    user_register_parser.add_argument('--dateOfBirth', action='store', type=str, required=True, help="date of birth to use for registration")
 
-    args = my_parser.parse_args()
+    # login command configuration
+    user_login_parser = subparser.add_parser("user_login", help="Command for user login")
+    user_login_parser.add_argument('--username', action='store', type=str, required=True, help="username to use for login")
+    user_login_parser.add_argument('--password', action='store', type=str, required=True, help="password to use for login")
+
+    # server command configuration
+    run_server_parser = subparser.add_parser("run_server", help="Command for running server")
+    run_server_parser.add_argument('--host', action='store', type=str, required=False, default='localhost', help='host address server to bind to')
+    run_server_parser.add_argument('--port', action='store', type=int, required=False, default=5050, help='port server to bind to.')
+
+    args = parser.parse_args()
 
     if args.action == "user_register":
         print(register(args.username, args.password, args.password2, args.dateOfBirth))
-    else:
+    elif args.action == "user_login":
         print(login(args.username, args.password))
+    elif args.action == "run_server":
+        server(args.host, args.port)
--- server.py
+++ server.py
@@ -1,6 +1,41 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# code borrowed from https://www.geeksforgeeks.org/building-a-basic-http-server-from-scratch-in-python/
+class BasicServer(BaseHTTPRequestHandler):
+
+    # creating a function for Get Request
+    def do_GET(self):
+
+        # Success Response --> 200
+        self.send_response(200)
+
+        # Type of file that we are using for creating our
+        # web server.
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+
+        # what we write in this function it gets visible on our
+        # web-server
+        self.wfile.write(
+            """
+            <html>
+                <head>
+                   <title>BookReview</title>
+                </head>
+                <body>
+                    <h1>Hello world</h1>
+                </body>
+            </html>
+            """.encode()
+        )
 
 def server(host, port):
     """
     Write code that starts server on host proviced and port. 
     """
-    pass
+    port = HTTPServer((host, port), BasicServer)
+
+    # this is used for running our
+    # server as long as we wish
+    # i.e. forever
+    port.serve_forever()
