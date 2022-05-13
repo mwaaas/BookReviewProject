66ee941fa675ad6bceacb2fbef12ed7aaea2efd0
Francis Mwangi <francismwangi152@gmail.com>
2022-05-13 09:27:15 +0000

    check code is upto date before running tests

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,17 +1,20 @@
 import subprocess
 
-def test_code_is_uptodate():
+def pytest_configure(config):
     branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode().strip()
 
     print(f"Current branch {branch}")
 
     subprocess.run(["git", "remote", "add", "original_repo", "https://github.com/SlashAirLearningWebDevelopment/BookReviewProject.git"])
-    branch_behind_original_repo_count = int(subprocess.check_output(["git", "rev-list", "--count", f"{branch}...original_repo/main"]).decode().strip())
+
+    # fetch original main branch 
+    subprocess.check_output(["git", "fetch", "original_repo", "main"])
+    
+    branch_behind_original_repo_count = int(subprocess.check_output(["git", "rev-list","--right-only", "--count", f"{branch}...original_repo/main"]).decode().strip())
 
     assert branch_behind_original_repo_count == 0, f"""
             Your repo branch {branch} is behind SlashAirLearningWebDevelopment/BookReviewProject 
             by {branch_behind_original_repo_count} commits
 
             Fix this by fetching upstream code and pulling it on your local machine. 
-            """ 
- 
+            """
