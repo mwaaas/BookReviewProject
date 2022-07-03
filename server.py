062c3cd661b9fc30a23cc3e2aff083d4f8c9761a
Francis Mwangi <francismwangi152@gmail.com>
2022-07-03 13:43:01 +0000

    implementing user register

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from users import register
 
 
 def server(host, port):
@@ -14,9 +15,36 @@ def server(host, port):
     while True:
         client_connection, client_address = listening_socket.accept()
 
-        request_data = client_connection.recv(1024)
+        request_data = client_connection.recv(1024).decode()
+        print("******************** start of request ****************")
 
-        print(f"request data: {request_data.decode()}")
+
+        if request_data in ('', '\r\n'):
+            client_connection.sendall(
+                f"HTTP/1.1 200 OK\r\nContent-Length: {len('')}\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n''".encode()
+            )
+            client_connection.close()
+            continue
+
+        request_data_list = request_data.split("\r\n")
+
+        method, url, http_version = request_data_list[0].split(" ")
+        request_arg_dict = {}
+
+        if "?" in url:
+            request_args = url[url.index("?") + 1 :]
+            for i in request_args.split("&"):
+                key, value = i.split('=')
+                request_arg_dict[key] = value
+
+
+
+        print(f"request data: {request_data}")
+
+        print(f"request arguments: {request_arg_dict}")
+        print("******************End of request*******************")
+        print()
+        print()
 
         html_page = """
         <html>
@@ -24,13 +52,56 @@ def server(host, port):
                 <title>BookReview</title>
             </head>
             <body>
-                <h1> Hello world </h1>
+                <h1> User registration </h1>
+
+                <form>
+                    <label for="username">Username</label><br>
+                    <input type="text" id="username", name="username"><br>
+
+                    <label for="password1">Password1</label><br>
+                    <input type="password" id="password1", name="password1"><br>
+
+                    <label for="password2">Password1</label><br>
+                    <input type="password" id="password2", name="password2"><br>
+
+                    <label for="dateOfBirth">Date of birth</label><br>
+                    <input type="date" id="dateOfBirth", name="dateOfBirth"><br>
+
+                    <input type="submit" id="submit">
+                </form>
             </body>
         </html>
         """
 
+        response_status = "200 OK"
+
+        if request_data.startswith("GET /?username") and request_arg_dict:
+            dateOfBirth = request_arg_dict['dateOfBirth']
+
+            year, month, day = dateOfBirth.split('-')
+            date_of_birth = f"{day}/{month}/{year}"
+
+            register_data = register(
+                request_arg_dict['username'],
+                request_arg_dict['password1'],
+                request_arg_dict['password2'],
+                date_of_birth)
+
+            print(f"register data: {register_data}")
+
+            response_status = "301 Moved Permanently\r\nLocation: /user/login"
+
+        if request_data.startswith("GET /user/login"):
+            html_page = """
+                <html>
+                    <body>
+                        <h1> You have successfully registered </h1>
+                    </body>
+                </html>
+            """
+
         client_connection.sendall(
-            f"HTTP/1.1 200 OK\r\nContent-Length: {len(html_page)}\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{html_page}".encode()
+            f"HTTP/1.1 {response_status}\r\nContent-Length: {len(html_page)}\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{html_page}".encode()
         )
         client_connection.close()
 
